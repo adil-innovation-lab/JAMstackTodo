5a077f67bf5cb8cccacfa06b85183359adac025b
adil-innovation-lab <khanadilaltaf@gmail.com>
2020-10-17 22:58:25 +0500

    Added Netlify Identity
    User Authentication with Netlify Identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
 
-export default props => (
+export default props => {
+   useEffect( () => {
+       netlifyIdentity.init({});
+   });
+   return (
     <Container>
         <Flex sx={{ flexDirection: "column", padding: 3 }}>
             <Heading as="h1">Todo App</Heading>
             <Button sx={{ marginTop: 2, color: 'black' }}
-                onClick= { () => {alert("Login")} }
+                onClick= { () => { netlifyIdentity.open() } }
             >
                 Login
             </Button>
         </Flex>
     </Container>
-);
+)}
